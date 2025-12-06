1d3524f7a63bbf46d93a7214c567af79f49f2579
lsebastin <lsebastin27@gmail.com>
2025-12-06 22:56:55 +0530

    Add files via upload
    day 1

---
+++ week1/FireFox.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class FireFox {public static void main(String[] args) {
+	double browserVersion = 100.2;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear = 1998;
+	char browserLogo = 'f';
+			System.out.println(browserVersion+"\n" + browserName + "\n"+ isVisible + "\n" + releaseYear + "\n" + browserLogo );
+
+}
+
+}
